f55151753b574c312dd2a35e109748f25ecb266e
MoizFar <moiz.0282@gmail.com>
2022-03-21 16:51:22 +0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: unnecessary_new, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'package:tictactoe/home_page.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(primaryColor: Colors.black),
+      home: new HomePage(),
+    );
+  }
+}
